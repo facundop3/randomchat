ea30ecf29c9a00063d51242c23e54b8e54da946a
Facundo Petre <facu1p3@gmail.com>
2019-05-25 17:13:14 -0300

    Fix

--- src/API.js
+++ src/API.js
@@ -1,36 +1,40 @@
-import openSocket from 'socket.io-client'
-const  socket = openSocket('http://localhost:3030')
+import openSocket from "socket.io-client";
+const socket = openSocket("http://randombla.herokuapp.com/");
 
 function suscribeToMessages(callback) {
-  socket.on('new-message', newMessage => callback(newMessage))
+  socket.on("new-message", newMessage => callback(newMessage));
 }
 
-function sendMessage(newMessage){
-  socket.emit('new-message', newMessage)
+function sendMessage(newMessage) {
+  socket.emit("new-message", newMessage);
 }
 
-function sendPrivateMessage(messageObj){
-  socket.emit('new-private-message', messageObj)
+function sendPrivateMessage(messageObj) {
+  socket.emit("new-private-message", messageObj);
 }
 
-function suscribeToPrivateMessages(callback){
-  socket.on('new-private-message', newPrivateMessages => callback(newPrivateMessages))
+function suscribeToPrivateMessages(callback) {
+  socket.on("new-private-message", newPrivateMessages =>
+    callback(newPrivateMessages)
+  );
 }
 
-function registerUser(username){
-  socket.emit('new-username', username)
+function registerUser(username) {
+  socket.emit("new-username", username);
 }
 
-function suscribeToUsers(callback){
-  socket.on('all-users', usersList =>{ 
-    callback(usersList)
-    console.log(usersList)
-  })
+function suscribeToUsers(callback) {
+  socket.on("all-users", usersList => {
+    callback(usersList);
+    console.log(usersList);
+  });
 }
 
-export default { suscribeToMessages,
-                 sendMessage, 
-                 registerUser, 
-                 suscribeToUsers,
-                 sendPrivateMessage,
-                 suscribeToPrivateMessages }
+export default {
+  suscribeToMessages,
+  sendMessage,
+  registerUser,
+  suscribeToUsers,
+  sendPrivateMessage,
+  suscribeToPrivateMessages
+};
